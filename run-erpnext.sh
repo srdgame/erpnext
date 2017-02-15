1e5e5b169a709c7457f81bc4b4e86fe9a5d43f6a
Dirk Chang <dirk@kooiot.com>
2017-02-15 19:15:36 +0800

    finished install erpnext

--- run-erpnext.sh
+++ run-erpnext.sh
@@ -1,2 +1,5 @@
 cd /home/frappe/frappe-bench
+/etc/init.d/cron start
+/etc/init.d/mysql start
+su frappe
 bench start
--- setup.sh
+++ setup.sh
@@ -6,6 +6,17 @@ apt-get install -y wget sudo python cron
 wget https://raw.githubusercontent.com/srdgame/bench/master/playbooks/install.py -O /home/frappe/install.py
 python /home/frappe/install.py --develop --user frappe
 
+chown -R frappe:frappe frappe-bench/sites
+su frappe -c "cd /home/frappe/frappe-bench && bench get-app erpnext https://github.com/frappe/erpnext"
+
+# Create site and install ERPNext
+#/etc/init.d/cron start
+#/etc/init.d/mysql start
+#su frappe -c "cd /home/frappe/frappe-bench && bench new-site site1.local --mariadb-root-password=frappe_pass --admin-password=admin"
+#su frappe -c "cd /home/frappe/frappe-bench && bench --site site1.local install-app erpnext"
+#/etc/init.d/mysql stop
+#/etc/init.d/cron stop
+
 # Remove dev pkgs
 apt-get -y remove build-essential python-dev python-software-properties libmariadbclient-dev libxslt1-dev libcrypto++-dev libssl-dev
 
